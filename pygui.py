32eb94f28fc8eee51707f868f71c16faaca4410f
liangdalong <liangdalong2010@gmail.com>
2018-10-26 10:03:40 +0800

    set label height and width

--- pygui.py
+++ pygui.py
@@ -1,17 +1,50 @@
 import sys
 from PyQt5.QtWidgets import *
 
-
-
-class GUi(QWidget):
+class GUi(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.win = QWidget()
-        self.win.setWindowTitle('窗口')
+        self.setWindowTitle('窗口')
+        self.statusBar().showMessage("文本栏状态")
         self.resize(400,100)
+        self.add_menu_and_statu()
+        self.add_position_layout()
+    def add_menu_and_statu(self):
+        self.statusBar().showMessage("文本状态栏")
+        #创建一个菜单栏
+        menu = self.menuBar()
+        #创建一个菜单
+        file_menu = menu.addMenu("文件")
+        #file_menu.addSepprator()
+        edit_menu = menu.addMenu("修改")
+
+        #创建一个行为
+        new_action = QAction('新的文件',self)
+        #更新状态栏文本
+        new_action.setStatusTip('打开新的文件')
+        # 添加一个行为到菜单
+        file_menu.addAction(new_action)
+
+        #创建退出行为
+        exit_action = QAction('退出',self)
+        #退出操作
+        exit_action.setStatusTip("点击退出应用程序")
+        #点击关闭程序
+        exit_action.triggered.connect(self.close)
+        #设置退出快捷键
+        exit_action.setShortcut('Ctrl+Q')
+        #添加退出行为到菜单上
+        file_menu.addAction(exit_action)
+
+    def add_position_layout(self):
+        label = QLabel("第一个标签",self)
+        label.move(10,20)
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
